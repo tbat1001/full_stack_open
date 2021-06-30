b008732aedba30c4c6bd1c514c8ee5b33f051ad2
tbat1001 <71069418+tbat1001@users.noreply.github.com>
2021-06-30 23:20:03 +0100

    Add files via upload

---
+++ part1/course_info/ex-1/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const App = () => {
+  const course = 'Half Stack application development'
+
+  const parts = [
+    'Fundamentals of React',
+    'Using props to pass data',
+    'State of a component'
+  ]
+  const exercises = [
+    10,
+    7,
+    14
+  ]
+
+  return (
+    <>
+      <Header course={course} />
+      <Content parts={parts} exercises={exercises} />
+      <Total exercises={exercises} />
+    </>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <>
+      <h1> {props.course} </h1>
+    </>
+  )
+}
+const Content = (props) => {
+  return (
+    <>
+      <p>{props.parts[0]} {props.exercises[0]}</p>
+      <p>{props.parts[1]} {props.exercises[1]}</p>
+      <p>{props.parts[2]} {props.exercises[2]}</p>
+    </>
+  )
+}
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+    </>
+  )
+}
+
+export default App
